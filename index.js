d1d1b00c7771d3a3a7178ec52443aa3ac06afb6e
Laura Berge <lauraeberge@gmail.com>
2019-12-09 02:29:22 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    let letters = string.split('')
+    let first = letters.shift()
+    letters.unshift(first.toUpperCase())
+    return letters.join('')
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9- ']+/g, '')
+  }
+
+  static titleize(string) {
+    let allLetters = string.split('')
+    let firstCap = allLetters.shift().toUpperCase();
+    allLetters.unshift(firstCap);
+    let words = allLetters.join('').split(' ');
+    return words.map(
+      function(word) {
+        const littles = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+        if (littles.includes(word)) {
+          return word
+        } else {
+          let letters = word.split('');
+          let first = letters.shift();
+          letters.unshift(first.toUpperCase());
+          return letters.join('');;
+        }
+      }
+    ).join(' ')
+  }
 }
