26395bdfae1c1223bc792c174e9594fa7caff0e2
KHong <albtrossfacefall@gmail.com>
2019-12-11 00:03:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^0-9a-zA-Z \'\-]/gi, '');
+  }
+
+  static titleize(string) {
+    const dontTitleize = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    const splitString = string.split(' ');
+    let newString = this.capitalize(splitString[0]);
+    newString = newString + " " + splitString.slice(1).map(function(w) {
+      if (!dontTitleize.includes(w)) {
+        return w[0].toUpperCase() + w.slice(1);
+      } else {
+        return w;
+      }
+    }).join(' ');
+    return newString;
+  }
 }
